9bcfb498f413570fe56e081a06e35a7f934d6d04
Soumyadeep Das <soumyadeep.dbsslg@gmail.com>
2022-01-21 18:10:38 +0530

    Designed home screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/constants.dart';
+import 'package:websafe_svg/websafe_svg.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          WebsafeSvg.asset(
+            'assets/images/back_home.svg',
+            height: MediaQuery.of(context).size.height,
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(
+                    flex: 2,
+                  ),
+                  Text(
+                    'It\'\s QuizVerse time!',
+                    style: Theme.of(context).textTheme.headline4!.copyWith(
+                        color: Colors.lightGreen, fontWeight: FontWeight.bold),
+                  ),
+                  
+                  Text('Enter your information'),
+                  Spacer(),
+                  TextField(
+                    decoration: InputDecoration(
+                      fillColor: Color(0xFF1C2341),
+                      filled: true,
+                      hintText: 'Full Name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(kDefaultPadding * 0.75),
+                      decoration: BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: Text(
+                        'Start Quiz',
+                        style: Theme.of(context)
+                            .textTheme
+                            .button!
+                            .copyWith(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  Spacer(
+                    flex: 2,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
